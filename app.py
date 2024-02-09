7b297368d33dd4166ee021fb85b0674a056a6f97
Jaksa Jovicic <jovicicjaksa@gmail.com>
2024-02-09 18:37:37 +0100

    Streamlit app

---
+++ app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import time
+from openai import OpenAI
+
+st.title("Simple chat")
+client = OpenAI()
+
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+def response_generator():
+    response = "Hello there! How can I assist you today?"
+     
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
