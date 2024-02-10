727923794004d5a749493c8743b5866f672781cb
Jaksa Jovicic <jovicicjaksa@gmail.com>
2024-02-10 11:28:09 +0100

    Streamlit version 1, annotations problem

--- app.py
+++ app.py
@@ -4,6 +4,26 @@ from openai import OpenAI
 
 st.title("Simple chat")
 client = OpenAI()
+assistants = ['asst_X5VLaEo73bi3fuH7DcrZnMF0',
+'asst_KH8fvndG7mfHmkcGw4mgzY0P',
+'asst_X0iM9rr8BcIaDCjQWGLLqTYk',
+'asst_97hGP77njU8eyxhKngUQ70m3',
+'asst_YL7bO35whowlEBv02wj3HrU5',
+'asst_DWyJySblBPbc4XenM1CFhTwb',
+'asst_zs7Ya0NvHUceAjf7Hgcr7Mnb',
+'asst_jBPdUspkKT7aYwqLFnh1m8Ht',
+'asst_75FXxglIh7vxlscAchB1q2Ok',
+'asst_VXHS7jZJrYC1nUAQzHxiNmOl',
+'asst_fiEHoZgjfRs3lQi1ozXnqmpE',
+'asst_ecJR8agsOGfVmMCefzRnrBuW',
+'asst_WGhiAfmQBjs6k8A1tnL5FYQA',
+'asst_sbxpi9R2Nji7Rwbz1nW5NJua',
+'asst_pDu1qz0HYBiWQGHUsB8DeORl',
+'asst_E7d11pusFqaIn6D2xRQA2pff',
+'asst_vFUYJOeB1DmE0Ipqat5aiVvz',
+'asst_m2BNvI9i076ViYm48SrwLvsh',
+'asst_KirbrNKyL9EqaiEwVLFdCGGj']
+thread = client.beta.threads.create()
 
 
 # Initialize chat history
@@ -23,18 +43,39 @@ def response_generator():
         time.sleep(0.05)
 
 if prompt := st.chat_input("What is up?"):
+    thread_message = client.beta.threads.messages.create(
+        thread_id = thread.id,
+        role = "user",
+        content = prompt,
+    )
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
-
-    with st.chat_message("assistant"):
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
+    
+    for assistant in assistants:
+        print(f"trying assistant {assistant}")
+        run = client.beta.threads.runs.create(
+            thread_id = thread.id,
+            assistant_id = assistant
         )
-        response = st.write_stream(stream)
-    st.session_state.messages.append({"role": "assistant", "content": response})
+        run = client.beta.threads.runs.retrieve(
+            thread_id=thread.id,
+            run_id=run.id
+        )
+
+        while run.status != "completed":
+            run = client.beta.threads.runs.retrieve(
+                thread_id=thread.id,
+                run_id=run.id
+            )
+            time.sleep(1)
+        
+        messages = client.beta.threads.messages.list(thread_id=thread.id)
+        last_msg = max(messages.data, key = lambda x: x.created_at)
+        response = last_msg.content[0].text.value
+        if response != "I'm sorry, I don't know the answer.":
+            print(f"response found with assistant {assistant}")
+            with st.chat_message("assistant"):
+                st.markdown(response)
+            st.session_state.messages.append({"role": "assistant", "content": response})
+            break
