672e8ea755a251acf3f4992f2b0f1f7985e0a6bc
Jaksa Jovicic <jovicicjaksa@gmail.com>
2024-03-21 22:49:15 +0100

    json -> sql final

--- helper.py
+++ helper.py
@@ -1,19 +1,14 @@
 from openai import OpenAI
-client = OpenAI()
 import time
 from typing import List, Optional, Union
 import re
-import datetime
-import dateparser
-from langchain.tools import BaseTool
-from langchain.callbacks.manager import AsyncCallbackManagerForToolRun, CallbackManagerForToolRun
 from langchain_openai import ChatOpenAI
-from langchain import hub
-from langchain.agents import AgentExecutor, create_react_agent
-import json
 from enum import Enum
-from prompts import classifier_prompt, count_prompt
-import sqlite3
+from prompts import classifier_prompt
+from langchain_community.utilities.sql_database import SQLDatabase
+from langchain_community.agent_toolkits import create_sql_agent
+from langchain_openai import ChatOpenAI
+
 
 """
 TODO
@@ -30,6 +25,8 @@ TODO
     - history?
     - asst_summary -> id_summary
 """
+client = OpenAI()
+
 gpt_3_5 = "gpt-3.5-turbo"
 gpt_4 = "gpt-4-turbo-preview"
 
@@ -38,11 +35,7 @@ class PromptClass(Enum):
     SUMMARIZE = "summarize"
     SEARCH = "search"
 
-class PromptSort(Enum):
-    ASC = "ascending"
-    DSC = "descending"
-
-def all_assistants():
+def all_assistants() -> List[str]:
     all_assistants = []
     first_assistants = client.beta.assistants.list(
             order="desc",
@@ -78,92 +71,17 @@ def classify(prompt: str) -> PromptClass:
         return PromptClass.SUMMARIZE
     return PromptClass.SEARCH
 
-def is_prompt_count(prompt: str) -> bool:
-    count_substrings = ["count", "how many" "total", "number of", "quantity of", "amount of", "tally", "enumerate", "calculate", "sum of", "total number", "aggregate", "tally up", "compute", "quantify"]
-    return any([s in prompt.lower() for s in count_substrings])
-
-def is_prompt_sort(prompt: str) -> Optional[PromptSort]:
-    sort_substrings = ["sort", "order", "arrange", "organize", "chronological"]
-    if not any([s in prompt.lower() for s in sort_substrings]):
-        return None
-    return PromptSort.DSC if "descending" in prompt else PromptSort.ASC
-
-def format_string(s):
-    relevant_keys = ['title', 'date', 'source', 'type', 'register_num']
-    for relevant_key in relevant_keys:
-        s = s.replace(relevant_key, f"e['{relevant_key}']")
-
-    types = ['lecture', 'article', 'patent']
-    for type_ in types:
-        s = s.replace(type_, f"'{type_}'")
-
-    s = s.replace("e['date']", "datetime.datetime.strptime(e[\'date\'], \'%Y-%m-%d\')")
-
-    def replace_four_digit_numbers(text):
-        pattern = r"\b(\d{4})\b"
-        return re.sub(pattern, r"dateparser.parse('\1')", text)
-
-    return replace_four_digit_numbers(s)
-
-results_list = []
-class TeslaToolGetDocuments(BaseTool):
-    name = "tesla_tool_get_documents"
-    description = """
-    Can be used to list all relevant Nikola Tesla document entries from a SQLite database based on a condition.
-    All entries are about documents written by Nikola Tesla, so don't consider him a source, sources are only considered for article publishers.
-    Fields that are available for search are title(str), date(str), source(str), type(str, possible values lecture, article, patent), register_num(str)
-    Operations that are available for search are "or, and, ==, <=, >=, <, >, !="
-    The tool takes as input a string representing conditions for search delimited with a single whitespace.
-    Example tool input:
-        type == lecture and date < 1905 and date > 1900
-    """
-    return_direct: bool = True
-
-    filename: str
-
-    def __init__(self, filename: str):
-        self.filename = filename
-
-    def __init__(self, filename: str):
-        self.filename = filename
-        self.conn = sqlite3.connect(self.filename)
-        self.cursor = self.conn.cursor()
-
-    def _run(self, tool_input: str, run_manager: Optional[CallbackManagerForToolRun] = None
-    ) -> str:
-        """Use the tool."""
-        parsed_condition = format_string(tool_input)
-        print(parsed_condition)
-        self.cursor.execute(f"SELECT * FROM documents WHERE {parsed_condition}")
-        results_list = self.cursor.fetchall()
-        return results_list
-
-    async def _arun(
-        self, tool_input: str, run_manager: Optional[AsyncCallbackManagerForToolRun] = None
-    ) -> str:
-        """Use the tool asynchronously."""
-        self._run(tool_input)
-
-custom_tool = TeslaToolGetDocuments(filename='your_database.db')
-llm = ChatOpenAI(model=gpt_4)
-tools = [custom_tool]
-prompt = hub.pull("hwchase17/react")
-agent = create_react_agent(llm, tools, prompt)
-agent_exec = AgentExecutor(agent=agent, tools=tools, verbose=True)
-asst_summary = json.load(open('assistant_summary.json'))
+db = SQLDatabase.from_uri("sqlite:///metadata.db")
+llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+sql_agent = create_sql_agent(llm, db=db, agent_type="openai-tools", verbose=True, top_k=200)
 
 def get_response(prompt: str) -> str:
     prompt_class = classify(prompt)
     print(f"Classified prompt {prompt} as {prompt_class.value}")
     if prompt_class == PromptClass.LOGIC:
-        result = agent_exec.invoke({"input": prompt})['output']
+        result = sql_agent.invoke({"input": prompt})['output']
         if not result:
             return "Sorry, no documents match your description."
-        if is_prompt_count(prompt):
-            result = result[0:10]
-            return f"Certainly, {len(result)} documents match your description. Here are some of them: {', '.join(map(str, [r['title'] for r in result]))}"
-        elif is_prompt_sort(prompt):
-            return f"Of course, here is a list of found documents that match your description in chronological order: \n{', '.join(map(str, [r['title'] for r in result])) }" 
         else:
             return f"Of course, here is a list of found documents that match your description: \n{', '.join(map(str, [r['title'] for r in result])) }"
     elif prompt_class == PromptClass.SUMMARIZE:
--- prompts.py
+++ prompts.py
@@ -16,22 +16,3 @@ Prompt: {0}
 
 Make sure to only answer with "Logic" or "Summarize" or "Search", nothing else.
 """
-
-# Revisit if possible?
-count_prompt = """
-Your task is to classify whether a given prompt requires counting elements of a certain list. List will be provided by another tool so no need to worry about that. Just output "1" if some sort of counting should be done and "0" if not. If a prompt asks for a list of documents but not the number of documents answer "0". Take the following examples:
-	- How many articles did Nikola Tesla file publish in New York Times? -> 1
-	- What did Nikola Tesla think about life on Mars? -> 0
-	- Summarize patent 505 -> 0
-    - can you give me the list of articles, patents and lectures of Nikola Tesla -> 0
-    - can you list Nikola Tesla lectures chronologically -> 0
-    - how many patent was issued by Nikola Tesla? -> 1
-    - how many lectures and articles were issued by Nikola Tesla between 1901 and 1905? -> 1
-    - list best Nikola Tesla articles -> 0
-    - List Nikola Tesla lectures after 1901 -> 0
-    - How many patents did Nikola Tesla file before 1905 -> 1
-
-Prompt: {0}
-
-Make sure to only answer with "1" if counting is needed or "0" if not.
-"""
