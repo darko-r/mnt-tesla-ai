e6fbb33d63c8c004b3a817be60e71282839185d5
Jaksa Jovicic <jovicicjaksa@gmail.com>
2024-04-02 21:58:33 +0200

    supervisor in app

--- helper.py
+++ helper.py
@@ -1,11 +1,21 @@
-from openai import OpenAI
-from prompts import sql_prompt, system_prompt
+import operator
+import functools
+from typing import Annotated, Sequence, TypedDict
+
+from prompts import sql_prompt, llm_prompt, base_supervisor_system_prompt, \
+    supervisor_system_prompt
+
 from langchain_openai import ChatOpenAI
-from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.output_parsers import BaseOutputParser
+from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_community.utilities.sql_database import SQLDatabase
-from langchain_community.agent_toolkits import create_sql_agent
-from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_community.agent_toolkits.sql.toolkit import SQLDatabaseToolkit
+from langchain.agents.agent import AgentExecutor, RunnableMultiActionAgent
+from langchain.agents import create_openai_tools_agent
+from langchain.chains import LLMChain
+from langchain.output_parsers.openai_functions import JsonOutputFunctionsParser
+from langgraph.graph import StateGraph, END
 
 """
 TODO
@@ -13,13 +23,14 @@ TODO
     done - integrade cond_parser 
     done - integrate classifier
     done - write get_file_name
+    done - supervisor agent
+    done - normal chat
     
     - parse and print citation
     - history
-    - adding additional tools
-    - supervisor agent
-    - normal chat
+    - RAG agent
     
+    not_needed - adding additional tools
     not_needed - search branch
     not_needed - asst_summary -> id_summary
     not_needed - try/catch around response generation
@@ -27,26 +38,118 @@ TODO
     not_needed - count output
     not_needed - multiple count
 """
-client = OpenAI()
 
+from langchain_openai import ChatOpenAI
+import os
 gpt_3_5 = "gpt-3.5-turbo"
 gpt_4 = "gpt-4-turbo-preview"
 
+llm3 = ChatOpenAI(model=gpt_3_5, temperature=0)
+llm4 = ChatOpenAI(model=gpt_4, temperature=0)
+llm = ChatOpenAI(model=gpt_3_5, temperature=0)
+
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_PROJECT"] = "Multi-agent Collaboration app"
+
+
+# sql agent
 db = SQLDatabase.from_uri("sqlite:///metadata.db")
-llm = ChatOpenAI(model=gpt_4, temperature=0)
 prompt_template = ChatPromptTemplate.from_messages([
-    ("system", system_prompt),
-    ("user", "{input}"),
+    ("system", sql_prompt),
+    MessagesPlaceholder(variable_name="messages"),
     MessagesPlaceholder(variable_name="agent_scratchpad"),
 ])
-sql_agent = create_sql_agent(llm, db=db, agent_type="openai-tools", verbose=True, top_k=200, prompt=prompt_template)
-message_history = ChatMessageHistory()
-agent_with_chat_history = RunnableWithMessageHistory(
-    sql_agent,
-    lambda session_id: message_history,
-    input_messages_key="input",
-    history_messages_key="chat_history",
+tools = tools = SQLDatabaseToolkit(llm=llm, db=db).get_tools()
+agent = RunnableMultiActionAgent(
+    runnable=create_openai_tools_agent(llm, tools, prompt_template),
+    input_keys_arg=["messages"],
+    return_keys_arg=["output"],
+)
+sql_agent_executor = AgentExecutor(
+    name="SQL Agent Executor",
+    agent=agent,
+    tools=tools,
+    callback_manager=None,
+    verbose=True,
+    max_iterations=15,
+    max_execution_time=None,
+    early_stopping_method="force",
 )
 
+
+# llm "agent"
+class CustomOutputParser(BaseOutputParser):
+    def parse(self, output):
+        return {'output': output}
+output_parser = CustomOutputParser()
+prompt_template = ChatPromptTemplate.from_messages([
+    ("system", llm_prompt),
+    MessagesPlaceholder(variable_name="messages"),
+])
+simple_llm_chain = LLMChain(
+    llm=llm3,
+    prompt=prompt_template,
+    output_parser=output_parser
+)
+
+
+# supervisor agent
+members = ["SQL", "CHAT"]
+options = ["FINISH"] + members
+function_def = {
+    "name": "route",
+    "description": "Select the next role.",
+    "parameters": {
+        "title": "routeSchema",
+        "type": "object",
+        "properties": {
+            "next": {
+                "title": "Next",
+                "anyOf": [
+                    {"enum": options},
+                ],
+            }
+        },
+        "required": ["next"],
+    },
+}
+prompt = ChatPromptTemplate.from_messages([
+    ("system", base_supervisor_system_prompt.format(members=members)),
+    MessagesPlaceholder(variable_name="messages"),
+    ("system", supervisor_system_prompt.format(options=options)),
+]).partial(options=str(options), members=", ".join(members))
+supervisor_chain = (
+    prompt
+    | llm4.bind_functions(functions=[function_def], function_call="route")
+    | JsonOutputFunctionsParser()
+)
+
+# graph definition
+class AgentState(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], operator.add]
+    next: str
+def agent_node(state, agent, name):
+    result = agent.invoke(state)
+    print(name, result)
+    if 'text' in result:
+        return {"messages": [HumanMessage(content=result['text']["output"], name=name)]}
+    return {"messages": [HumanMessage(content=result["output"], name=name)]}
+sql_node = functools.partial(agent_node, agent=sql_agent_executor, name="SQL")
+chat_node = functools.partial(agent_node, agent=simple_llm_chain, name="CHAT")
+workflow = StateGraph(AgentState)
+workflow.add_node("SQL", sql_node)
+workflow.add_node("CHAT", chat_node)
+workflow.add_node("supervisor", supervisor_chain)
+for member in members:
+    workflow.add_edge(member, "supervisor")
+conditional_map = {k: k for k in members}
+conditional_map["FINISH"] = END
+workflow.add_conditional_edges("supervisor", lambda x: x["next"], conditional_map)
+workflow.set_entry_point("supervisor")
+graph = workflow.compile()
+
+# function to get response
 def get_response(prompt: str) -> str:
-    return agent_with_chat_history.invoke({"input": sql_prompt.format(prompt)}, config={"configurable": {"session_id": "<foo>"}})['output']
+    o = graph.invoke({"messages": [HumanMessage(content=prompt)]})
+    return o['messages'][-1].content
+    # return agent_with_chat_history.invoke({"input": sql_prompt.format(prompt)}, config={"configurable": {"session_id": "<foo>"}})['output']
--- prompts.py
+++ prompts.py
@@ -9,9 +9,26 @@ Type can be 'lecture', 'article', or 'patent'.
 
 sql_prompt = """
 You have a single table called metadata that contains entries of files by Nikola Tesla, columns of interest are: 'index', 'id', 'title', 'date', 'type', 'source', 'register_num', 'summary'.
-Nikola Tesla is the author of all entries, so he should not be used in queries.
+Nikola Tesla is the author of all entries, so he should not be used in queries, or to parse titles.
 Source is available only for articles, and is the publisher of the article.
 Register_num is available only for patents.
-
-Based on that answer the following question: {0}
+Type can be 'lecture', 'article', or 'patent'.
 """
+
+llm_prompt = """
+You are an AI assistant.
+"""
+
+base_supervisor_system_prompt = """
+You are a supervisor tasked with managing a conversation between the
+following workers: {members}. SQL has an acces to a database containing metadata of Nikola Tesla's works so it can answer questions like how many articles did Nikola Tesla write etc... but it does not have access to his works so it cannot answer questions about specific documents. 
+CHAT has access to a GPT-4 model, it should answer questions that do not need any data. 
+Given the following user request,
+respond with the worker to act next. Each worker will perform a
+task and respond with their results and status. When finished,
+respond with FINISH.
+"""
+
+supervisor_system_prompt = """Given the conversation above, who should act next?
+SQL should be selected if the question is about metadata of Nikola Tesla's works, CHAT should be selected if the question does not need any data. Otherwise, or if there is a loop in the conversation, feel free to select FINISH.
+Or should we FINISH? Select one of: {options}"""
