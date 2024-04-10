5cab95b2ca73166b11d518c1c09b710f7c86e10e
Jaksa Jovicic <jovicicjaksa@gmail.com>
2024-04-10 19:41:50 +0200

    patent location

--- prompts.py
+++ prompts.py
@@ -1,17 +1,10 @@
-system_prompt = """
-You have a single table called metadata that contains entries of files by Nikola Tesla, columns of interest are: 'index', 'id', 'title', 'date', 'type', 'source', 'register_num', 'summary'.
-Nikola Tesla is the author of all entries, so he should not be used in queries, or to parse titles.
-Source is available only for articles, and is the publisher of the article.
-Register_num is available only for patents.
-Type can be 'lecture', 'article', or 'patent'.
-
-"""
-
 sql_prompt = """
-You have a single table called metadata that contains entries of files by Nikola Tesla, columns of interest are: 'index', 'id', 'title', 'date', 'type', 'source', 'register_num', 'summary'.
+You have a single table called metadata that contains entries of files by Nikola Tesla, columns of interest are: 'index', 'id', 'title', 'date', 'type', 'source', 'register_num', 'summary', 'country'.
 Nikola Tesla is the author of all entries, so he should not be used in queries, or to parse titles.
 Source is available only for articles, and is the publisher of the article.
 Register_num is available only for patents.
+American patents have country value United States of America.
+Australian patents have country values Australia, Victoria or New South Wales.
 Type can be 'lecture', 'article', or 'patent'.
 """
 
